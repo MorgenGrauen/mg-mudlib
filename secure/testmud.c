66656a847403758d4f38044c5722ccf686879d1b
Zesstra@Morgengrauen <zesstra@zesstra.de>
2016-07-20 23:22:47 +0200

    Initialisierung von Files in Homemuds
    Jof inklusive Workroom sollte im Homemud existieren. Sie werden jetzt 
    waehrend des preloads erstellt, wenn noetig. Das Savefile unter
    /secure/save/ wird vom Objekt /secure/testmud mittels save_object()
    erstellt.
    
    Change-Id: I8ebf6ad91212b36c85ca7f4819916ef8fca2117d

---
+++ secure/testmud.c
@@ -0,0 +1,40 @@
+// MorgenGrauen MUDlib
+#pragma strong_types,save_types,rtt_checks
+#pragma no_clone,no_inherit,no_shadow
+#pragma pedantic, range_check
+
+#include <files.h>
+
+// Variablen fuer das Savefile von Jof, was dieses Objekt schreibt.
+string name = "jof";
+string password = "";
+int level = 100;
+string shell = "/std/shells/magier";
+
+public varargs int remove(int s)
+{
+  destruct(this_object());
+  return 1;
+}
+
+protected void create()
+{
+# ifndef __TESTMUD__
+  remove();
+  return;
+# endif
+  seteuid(getuid(this_object()));
+  if(file_size("/players/jof") == FSIZE_NOFILE)
+  {
+    mkdir("/players/jof");
+  }
+  if(file_size("/players/jof/workroom") == FSIZE_NOFILE)
+  {
+    copy_file("/std/def_workroom.c","/players/jof/workroom.c");
+  }
+  if(file_size("/secure/save/j/jof.o") == FSIZE_NOFILE)
+  {
+    save_object("/secure/save/j/jof");
+  }
+  remove();
+}
