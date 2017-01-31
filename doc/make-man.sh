1cf5bb7d98f4f26e8be3e976ff0660d33631093f
Zesstra <zesstra@zesstra.de>
2017-01-31 12:02:09 +0100

    sphinx nutzen zum erstellen der Textmanpages
    Change-Id: I5cccacd7d6759d46143c31803df404705d0e9e83

--- doc/make-man.sh
+++ doc/make-man.sh
@@ -4,7 +4,9 @@ export MUDHOME=/home/mud
 export MUDLIB=$MUDHOME/mudlib
 export MDOCDIR=$MUDLIB/doc
 export MRSTDIR=$MDOCDIR/sphinx
-export MHTMLDIR=$MDOCDIR/sphinx/html
+export MHTMLDIR=$MDOCDIR/sphinx/_build/html
+export MTEXTDIR=$MDOCDIR/sphinx/_build/text
+export MMANDIR=$MDOCDIR/sphinx/man/
 export MANWIDTH=78
 
 #rst2man < asin.rst | man -7 -P cat -l -
@@ -12,22 +14,24 @@ export MANWIDTH=78
 cd $MRSTDIR
 
 make html
+make text
 
-SOURCES=`find . -type f -name asin.rst`
+cd $MTEXTDIR
+SOURCES=`find . -type f -name \*.txt`
 for FILE in $SOURCES
 do
   # skip leading ./
-#DIR=${DIR:2}
-  echo "Processing $FILE..."
+#  DIR=${DIR:2}
+#  echo "Processing $FILE..."
   DIR=`dirname ${FILE}`
-  BASE=`basename ${FILE} .rst`
+  BASE=`basename ${FILE} .txt`
 #  mkdir -p ${MHTMLDIR}/${DIR}
 #SIG=`git log -n 1 --date=iso-local --pretty="%cd von %an" -- $FILE`
 #  sed -e "s/\\\$LastChange\\\$/$SIG/g" $FILE | rst2html > ${MHTMLDIR}/${DIR}/${BASE}.html
 #  rst2html $FILE ${MHTMLDIR}/${DIR}/${BASE}.html
 #  if [ $? -eq 0 ] ; then
-#    mkdir -p ${MDOCDIR}/${DIR}
-#    rst2man < $FILE | man -7 -P cat -l - > ${MDOCDIR}/${DIR}/${BASE}
+    mkdir -p ${MMANDIR}/${DIR}
+    cp $FILE ${MMANDIR}/${DIR}/${BASE}
 #  fi
 done
 
