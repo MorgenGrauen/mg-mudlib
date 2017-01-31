141f717334258b6efa478baf8e2aafce7b5e5200
Zesstra <zesstra@zesstra.de>
2017-01-31 10:42:57 +0100

    Script zur Erzeugung von Manpages+HTML
    Erstellung der Manpages zur Zeit noch inaktiv.
    
    Change-Id: I1e7853e0a793451ae689d91cacb4fa7e71293daa

--- doc/make-man.sh
+++ doc/make-man.sh
@@ -3,16 +3,17 @@
 export MUDHOME=/home/mud
 export MUDLIB=$MUDHOME/mudlib
 export MDOCDIR=$MUDLIB/doc
-export MRSTDIR=$MDOCDIR/rst
-export MHTMLDIR=$MDOCDIR/html
+export MRSTDIR=$MDOCDIR/sphinx
+export MHTMLDIR=$MDOCDIR/sphinx/html
 export MANWIDTH=78
 
 #rst2man < asin.rst | man -7 -P cat -l -
 
 cd $MRSTDIR
 
-SOURCES=`find . -type f -name asin.rst`
+make html
 
+SOURCES=`find . -type f -name asin.rst`
 for FILE in $SOURCES
 do
   # skip leading ./
@@ -20,10 +21,13 @@ do
   echo "Processing $FILE..."
   DIR=`dirname ${FILE}`
   BASE=`basename ${FILE} .rst`
-  mkdir -p ${MHTMLDIR}/${DIR}
-  rst2html $FILE ${MHTMLDIR}/${DIR}/${BASE}.html
-  if [ $? -eq 0 ] ; then
-    mkdir -p ${MDOCDIR}/${DIR}
+#  mkdir -p ${MHTMLDIR}/${DIR}
+#SIG=`git log -n 1 --date=iso-local --pretty="%cd von %an" -- $FILE`
+#  sed -e "s/\\\$LastChange\\\$/$SIG/g" $FILE | rst2html > ${MHTMLDIR}/${DIR}/${BASE}.html
+#  rst2html $FILE ${MHTMLDIR}/${DIR}/${BASE}.html
+#  if [ $? -eq 0 ] ; then
+#    mkdir -p ${MDOCDIR}/${DIR}
 #    rst2man < $FILE | man -7 -P cat -l - > ${MDOCDIR}/${DIR}/${BASE}
-  fi
+#  fi
 done
+
