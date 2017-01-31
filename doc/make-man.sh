0516f5d49c754ae39b935f3eb01561110b764320
Zesstra <zesstra@zesstra.de>
2017-01-31 12:05:00 +0100

    Kein HTML mehr erstellen
    HTML brauchts mudintern nicht, also nur text erstellen
    Ausserdem etwas aufgeraeumt.
    
    Change-Id: I4483d61214ac37fb84998c21e204ba25bd991e83

--- doc/make-man.sh
+++ doc/make-man.sh
@@ -9,29 +9,18 @@ export MTEXTDIR=$MDOCDIR/sphinx/_build/text
 export MMANDIR=$MDOCDIR/sphinx/man/
 export MANWIDTH=78
 
-#rst2man < asin.rst | man -7 -P cat -l -
-
 cd $MRSTDIR
 
-make html
 make text
 
 cd $MTEXTDIR
 SOURCES=`find . -type f -name \*.txt`
 for FILE in $SOURCES
 do
-  # skip leading ./
-#  DIR=${DIR:2}
 #  echo "Processing $FILE..."
   DIR=`dirname ${FILE}`
   BASE=`basename ${FILE} .txt`
-#  mkdir -p ${MHTMLDIR}/${DIR}
-#SIG=`git log -n 1 --date=iso-local --pretty="%cd von %an" -- $FILE`
-#  sed -e "s/\\\$LastChange\\\$/$SIG/g" $FILE | rst2html > ${MHTMLDIR}/${DIR}/${BASE}.html
-#  rst2html $FILE ${MHTMLDIR}/${DIR}/${BASE}.html
-#  if [ $? -eq 0 ] ; then
-    mkdir -p ${MMANDIR}/${DIR}
-    cp $FILE ${MMANDIR}/${DIR}/${BASE}
-#  fi
+  mkdir -p ${MMANDIR}/${DIR}
+  cp $FILE ${MMANDIR}/${DIR}/${BASE}
 done
 
