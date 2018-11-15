c3c8307651c1587acb28a3bd351c21a0e0914a79
Zesstra <zesstra@zesstra.de>
2018-11-15 22:29:06 +0100

    Leere Anfrage korrekt mit Fehlermeldung behandeln.
    Change-Id: Ie9242448f32327c8d9e20105ad285c7d587a1329

--- secure/udp/finger.c
+++ secure/udp/finger.c
@@ -16,12 +16,18 @@ string last_finger;
 
 void udp_finger(mapping data)
 {
+  string response;
+  if (stringp(data[DATA]) && sizeof(data[DATA]))
+    response="/p/daemon/finger"->finger_single(data[DATA]);
+  else
+    response="Du solltest schon sagen, wer Dich interessiert!";
+
   last_finger=capitalize(to_string(data[SENDER]))+"@"+data[NAME];
   INETD->_send_udp(data[NAME], ([
 	REQUEST: REPLY,
 	RECIPIENT: data[SENDER],
 	ID: data[ID],
-	DATA: "/p/daemon/finger"->finger_single(data[DATA])
+	DATA: response
     ]) );
 }
 
