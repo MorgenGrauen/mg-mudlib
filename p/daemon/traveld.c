f15383d44f3170b63117d42797d816c0c71351ff
Zesstra <zesstra@zesstra.de>
2019-07-19 16:35:06 +0200

    Transporteraktivierung im reset entfernt.
    Inzwischen sind die Transporter hoffentlich zuverlaessig
    genug, damit diese Kruecke nicht mehr gebraucht wird.
    Zudem fallen Fehler jetzt hoffentlich auch auf.
    
    Change-Id: I42a6c2d379ecda05c45b1d80271a54f9dd9f13e4

--- p/daemon/traveld.c
+++ p/daemon/traveld.c
@@ -108,20 +108,7 @@ protected void create()
 
 public void reset()
 {
-  set_next_reset(10800);
-  // Als Wuergaround fuer das Problem, dass selten genutzte Transporter in
-  // spielerunzugaenglichen Raeumen stehen bleiben und der spielerzugaengliche Raum
-  // mangels Benutzung ausgeswappt wird, lassen wir hier alle paar Stunden mal
-  // alle bekannten Transporter loslaufen. Ohne Spielerkontakt bleiben sie
-  // nach einem Routenumlauf wieder stehen.
-  // TODO: Problem "richtig" loesen.
-  foreach(string ship: transporters)
-  {
-    // ja, geht nur fuer Blueprints. BTW: hart zerstoerte Transporter, die
-    // sich nicht abgemeldet haben, werden jetzt neugeladen. Das muss nicht
-    // immer gut sein, ist momentan aber sogar willkommen.
-    ship->Continue();
-  }
+
 }
 
 varargs int remove(int s)
