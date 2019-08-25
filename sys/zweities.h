8b50ee15d9cedbf743a83380849cd1f492e17a14
Zesstra <zesstra@zesstra.de>
2019-08-25 17:56:48 +0200

    sym. Konstanten fuer die ZweitieDB hinzugefuegt.
    Erstmal nur den Pfad davon.
    
    Change-Id: I3115c671dcecee214c7a22cab6a6c770cd6fc75c

---
+++ sys/zweities.h
@@ -0,0 +1,12 @@
+// MorgenGrauen MUDlib
+//
+// zweities.h - sym. Konstanten fuer die ZweitieDB
+//
+
+#ifndef _ZWEITIES_H_
+#define _ZWEITIES_H_
+
+#define ZWEITIEDB "/secure/zweities"
+
+#endif /* _ZWEITIES_H_ */
+
