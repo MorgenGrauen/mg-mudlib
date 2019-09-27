0a6b0889200320cde626eb36380a53ec6f393d52
Zesstra <zesstra@zesstra.de>
2019-09-27 16:17:09 +0200

    Update Headerfiles aus LDMud 3.6
    Change-Id: Ibf755506cb765d2e3b600c36dec9bfdf4a7ce0c8

--- sys/configuration.h
+++ sys/configuration.h
@@ -18,6 +18,7 @@
 #define IC_PROMPT                        9
 #define IC_MAX_COMMANDS                 10
 #define IC_MODIFY_COMMAND               11
+#define IC_ENCODING                     12
 
 /* Possible options for configure_object().
  */
@@ -37,5 +38,26 @@
 #define DC_EXTRA_WIZINFO_SIZE            7
 #define DC_DEFAULT_RUNTIME_LIMITS        8
 #define DC_SWAP_COMPACT_MODE             9
+#define DC_SWAP_TIME                     10
+#define DC_SWAP_VAR_TIME                 11
+#define DC_CLEANUP_TIME                  12
+#define DC_RESET_TIME                    13
+#define DC_DEBUG_FILE                    14
+#define DC_FILESYSTEM_ENCODING           15
+
+#define DC_SIGACTION_SIGHUP              20
+#define DC_SIGACTION_SIGINT              21
+#define DC_SIGACTION_SIGUSR1             22
+#define DC_SIGACTION_SIGUSR2             23
+
+/* Values for the DC_SIGACTION_SIG* options:
+ */
+#define DCS_DEFAULT                      0
+#define DCS_IGNORE                       1
+#define DCS_TERMINATE                    2
+#define DCS_SHUTDOWN                     3
+#define DCS_INFORM_MASTER                4
+#define DCS_RELOAD_MASTER                5
+#define DCS_THROW_EXCEPTION              6
 
 #endif /* LPC_CONFIGURATION_H_ */
