29a3aa9a466bf101af486f5a0119b2397afb894d
Zesstra <zesstra@zesstra.de>
2021-06-19 12:16:31 +0200

    Define fuer Versionscounter
    Change-Id: I1e4be3d942a87454f2ab435a2a6f309880fee710

--- std/player/protocols/mudlet_gui.c
+++ std/player/protocols/mudlet_gui.c
@@ -1,9 +1,12 @@
 #pragma strong_types,rtt_checks,no_clone,no_inherit,no_shadow
 
+CURRENT_VERSION 2
+
 // Liefert die URL zum Download und die aktuelle Version des
 // MorgenGrauen.mpackage fuer Mudlet.
 public <int|string>* current_version()
 {
-  return ({"https://mg.mud.de/download/MorgenGrauen.mpackage", 2});
+  return ({"https://mg.mud.de/download/MorgenGrauen.mpackage",
+           CURRENT_VERSION});
 }
 
