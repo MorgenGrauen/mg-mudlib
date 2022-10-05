1e1fde44929638826d958ba77db16083875506f8
Zesstra <zesstra@zesstra.de>
2022-10-05 23:36:32 +0200

    Magieransprechpartner geaendert.
    Wargon und Wurzel sind lang nicht mehr zustaendig.
    
    Change-Id: Ic79accc6455be605308726e8ccd23ccb17ab4235

--- d/seher/haeuser/abstellraum.c
+++ d/seher/haeuser/abstellraum.c
@@ -1,13 +1,15 @@
 inherit "/std/room";
-#include "/d/unterwelt/wurzel/wurzel.h" //noch zu aendern...
 
-create() {
+#include <properties.h>
+#define BS(x)          break_string(x,79)
+
+protected void create() {
   ::create();
   SetProp(P_HAUS_ERLAUBT,1);
   SetProp(P_INT_SHORT,"Der Abstellraum fuer heimatlose Seherhaeuser");
   SetProp(P_INT_LONG,BS("Hier landen Seherhaeuser, die keinen Platz mehr haben. Wenn "
   +"Dein Haus hier steht, wende Dich am besten bald an den zustaendigen Magier fuer die "
   +"Haeuser.\n"));
-  AddDetail("magier","Wurzel oder Wargon kuemmern sich momentan um die Haeuser.\n");
+  AddDetail("magier","Zook kuemmert sich momentan um die Haeuser.\n");
   AddExit("norden","/gilden/abenteurer");
 }
